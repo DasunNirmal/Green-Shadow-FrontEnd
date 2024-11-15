17c7b0f067440989cee4d942cce4113a09d745f8
DasunNirmal <dasun200301@gmail.com>
2024-11-16 00:40:06 +0530

    login page done

--- assets/js/loginpage.js
+++ assets/js/loginpage.js
@@ -1,7 +1,29 @@
 $(document).ready(function(){
    $('#email-input').focus(function(){
-       $('#email-logo').css({
-           display: 'none'
-       });
-   })
+       $('#email-logo').addClass('focused');
+   });
+   $('#email-input').blur(function () {
+        $('#email-logo').removeClass('focused');
+   });
+   $('#email-input').on('input', function () {
+        if ($(this).val().length > 30) {
+            $('#email-logo').addClass('vanished');
+        } else {
+            $('#email-logo').removeClass('vanished');
+        }
+   });
+
+    $('#password-input').focus(function(){
+        $('#password-logo').addClass('focused');
+    });
+    $('#password-input').blur(function () {
+        $('#password-logo').removeClass('focused');
+    });
+    $('#password-input').on('input', function () {
+        if ($(this).val().length > 30) {
+            $('#password-logo').addClass('vanished');
+        } else {
+            $('#password-logo').removeClass('vanished');
+        }
+    });
 });

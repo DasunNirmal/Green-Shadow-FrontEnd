499d080728694bec2a266af9ea4578431d08057e
DasunNirmal <dasun200301@gmail.com>
2024-12-01 20:57:12 +0530

    get all done

--- controller/CropController.js
+++ controller/CropController.js
@@ -1,4 +1,44 @@
 $(document).ready(function () {
+    loadCropTable();
+
+    function loadCropTable() {
+        $("#crops-table-tb").empty();
+
+        $.ajax({
+            url: 'http://localhost:8081/greenShadow/api/v1/crop',
+            type: 'GET',
+            dataType: 'json',
+            success: function(res) {
+                console.log(res);
+                if (Array.isArray(res)) {
+                    res.forEach(function(crop) {
+                        var cropRecord = `
+                        <tr>
+                            <td class="c-crop_code">${crop.crop_code}</td>
+                            <td class="c-common_name">${crop.common_name}</td>
+                            <td class="c-fscientific_name">${crop.scientific_name}</td>
+                            <td class="c-category">${crop.category}</td>
+                            <td class="c-season">${crop.season}</td>
+                            <td class="c-field_code">${crop.field_code}</td>
+                            <td class="c-img"><img src="data:image/png;base64,${crop.img}" width="150px"></td>
+                        </tr>`;
+                        $('#crops-table-tb').append(cropRecord);
+                    });
+                    let count = 0;
+                    for (let i = 0; i < res.length; i++) {
+                        if (res[i] != null) {
+                            count++;
+                        }
+                    }
+                } else {
+                    console.log('No crop data found or incorrect response format.');
+                }
+            },
+            error: function(res) {
+                console.error('Error loading crop data:', res);
+            }
+        });
+    }
 
     $('#btnSearchField').on('click', function() {
         const searchQuery = $('#txtSearchField').val();
@@ -64,6 +104,7 @@ $(document).ready(function () {
             success: (res) => {
                 console.log(res);
                 console.log("crop saved");
+                loadCropTable();
             },
             error: (res) => {
                 console.error(res);

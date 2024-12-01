beffc57be651a30e9e939807cf8f611110705b16
DasunNirmal <dasun200301@gmail.com>
2024-12-01 13:53:15 +0530

    get and table on clck done

--- controller/StaffController.js
+++ controller/StaffController.js
@@ -1,5 +1,90 @@
 $(document).ready(function () {
-    
+    var recordIndexStaff;
+    loadStaffTable();
+
+    function loadStaffTable() {
+        $("#staff-table-tb").empty();
+
+        $.ajax({
+            url: 'http://localhost:8081/greenShadow/api/v1/staff',
+            type: 'GET',
+            dataType: 'json',
+            success: function(res) {
+                console.log(res);
+                if (Array.isArray(res)) {
+                    res.forEach(function(staff) {
+                        var staffRecord = `
+                        <tr>
+                            <td class="s-staff-id">${staff.staff_id}</td>
+                            <td class="s-first-name">${staff.first_name}</td>
+                            <td class="s-last-name">${staff.last_name}</td>
+                            <td class="s-email">${staff.email}</td>
+                            <td class="s-phone-no">${staff.phone_no}</td>
+                            <td class="s-designation">${staff.designation}</td>
+                            <td class="s-dob">${staff.dob}</td>
+                            <td class="s-role">${staff.role}</td>
+                            <td class="s-gender">${staff.gender}</td>
+                            <td class="s-joined-date">${staff.joined_date}</td>
+                            <td class="s-address-01">${staff.address_01}</td>
+                            <td class="s-address-02">${staff.address_02}</td>
+                            <td class="s-address-03">${staff.address_03}</td>
+                            <td class="s-address-04">${staff.address_04}</td>
+                            <td class="s-address-05">${staff.address_05}</td>
+                        </tr>`;
+                        $('#staff-table-tb').append(staffRecord);
+                    });
+                    let count = 0;
+                    for (let i = 0; i < res.length; i++) {
+                        if (res[i] != null) {
+                            count++;
+                        }
+                    }
+                } else {
+                    console.log('No staff data found or incorrect response format.');
+                }
+            },
+            error: function(res) {
+                console.error('Error loading staff data:', res);
+            }
+        });
+    }
+
+    $('#staff-table-tb').on('click','tr',function () {
+        recordIndexStaff = $(this).index();
+
+        var staff_id = $(this).find(".s-staff-id").text();
+        var first_name = $(this).find(".s-first-name").text();
+        var last_name = $(this).find(".s-last-name").text();
+        var email = $(this).find(".s-email").text();
+        var phone_no = $(this).find(".s-phone-no").text();
+        var designation = $(this).find(".s-designation").text();
+        var dob = $(this).find(".s-dob").text();
+        var role = $(this).find(".s-role").text();
+        var gender = $(this).find(".s-gender").text();
+        var joined_date = $(this).find(".s-joined-date").text();
+        var address_01 = $(this).find(".s-address-01").text();
+        var address_02 = $(this).find(".s-address-02").text();
+        var address_03 = $(this).find(".s-address-03").text();
+        var address_04 = $(this).find(".s-address-04").text();
+        var address_05 = $(this).find(".s-address-05").text();
+
+        $('#txtMemberID').val(staff_id);
+        $('#txtFirstName').val(first_name);
+        $('#txtLastName').val(last_name);
+        $('#txtDesignation').val(designation);
+        $('#txtEmail').val(email);
+        $('#txtRole').val(role);
+        $('#txtGender').val(gender);
+        $('#txtJoinedDate').val(joined_date);
+        $('#txtDateOfBirth').val(dob);
+        $('#txtAddressLine1').val(address_01);
+        $('#txtAddressLine2').val(address_02);
+        $('#txtAddressLine3').val(address_03);
+        $('#txtAddressLine4').val(address_04);
+        $('#txtAddressLine5').val(address_05);
+        $('#txtPhoneNumber').val(phone_no);
+    });
+
     $('#save-staff').on('click', () => {
         var staff_id = $('#txtMemberID').val();
         var first_name = $('#txtFirstName').val();
@@ -45,6 +130,7 @@ $(document).ready(function () {
             contentType: 'application/json',
             success: (res) => {
                 console.log(JSON.stringify(res));
+                loadStaffTable();
             },
             error: (res) => {
                 console.error(res);

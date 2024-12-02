7b47003134e649691175f2e71ecdbde48e2abff5
DasunNirmal <dasun200301@gmail.com>
2024-12-02 19:54:07 +0530

    get all done with table on click

--- controller/VehicleController.js
+++ controller/VehicleController.js
@@ -1,5 +1,82 @@
 $(document).ready(function(){
+    loadVehicleTable();
+    var recordIndexVehicle;
 
+    function loadVehicleTable() {
+        $("#vehicles-table-tb").empty();
+
+        $.ajax({
+            url: 'http://localhost:8081/greenShadow/api/v1/vehicle',
+            type: 'GET',
+            dataType: 'json',
+            success: function(res) {
+                console.log(res);
+                if (Array.isArray(res)) {
+                    res.forEach(function(vehicle) {
+                        var staff_id = vehicle.staff_id == null ? "Not Available" : vehicle.staff_id;
+                        var vehicleRecord = `
+                        <tr>
+                            <td class="v-vehicle_code">${vehicle.vehicle_code}</td>
+                            <td class="v-license_plate">${vehicle.license_plate}</td>
+                            <td class="v-fuel_type">${vehicle.fuel_type}</td>
+                            <td class="v-vehicle_category">${vehicle.vehicle_category}</td>
+                            <td class="v-remarks">${vehicle.remarks}</td>
+                            <td class="v-status">${vehicle.status}</td>
+                            <td class="v-staff_id">${staff_id}</td>
+                            <td class="v-first_name">${vehicle.first_name}</td>
+                            <td class="v-role">${vehicle.role}</td>
+                            <td class="v-phone_no">${vehicle.phone_no}</td>
+                            <td class="v-email">${vehicle.email}</td>
+                        </tr>`;
+                        $('#vehicles-table-tb').append(vehicleRecord);
+                    });
+                    let count = 0;
+                    for (let i = 0; i < res.length; i++) {
+                        if (res[i] != null) {
+                            count++;
+                        }
+                    }
+                } else {
+                    console.log('No vehicle data found or incorrect response format.');
+                }
+            },
+            error: function(res) {
+                console.error('Error loading vehicle data:', res);
+            }
+        });
+    }
+
+    $('#vehicles-table-tb').on('click','tr',function () {
+        recordIndexVehicle = $(this).index();
+
+        var vehicle_code = $(this).find(".v-vehicle_code").text();
+        var license_plate = $(this).find(".v-license_plate").text();
+        var fuel_type = $(this).find(".v-fuel_type").text();
+        var vehicle_category = $(this).find(".v-vehicle_category").text();
+        var remarks = $(this).find(".v-remarks").text();
+        var status = $(this).find(".v-status").text();
+        var staff_id = $(this).find(".v-staff_id").text();
+        var first_name = $(this).find(".v-first_name").text();
+        var role = $(this).find(".v-role").text();
+        var phone_no = $(this).find(".v-phone_no").text();
+        var email = $(this).find(".v-email").text();
+
+        var isAvailable = status === "Available";
+
+        $('#txtVehicleRole').prop('disabled', !isAvailable).val(role);
+        $('#txtVehicleFirstName').prop('disabled', !isAvailable).val(first_name);
+        $('#txtVehiclePhoneNumber').prop('disabled', !isAvailable).val(phone_no);
+        $('#txtVehicleEmail').prop('disabled', !isAvailable).val(email);
+        $('#txtVehicleMemberID').prop('disabled', !isAvailable).val(staff_id);
+        $('#txtSearchEmploys').prop('disabled', !isAvailable);
+        $('#btnSearchEmploys').prop('disabled', !isAvailable);
+        $('#txtVehicleCode').val(vehicle_code);
+        $('#txtLicensePlate').val(license_plate);
+        $('#txtFuelType').val(fuel_type);
+        $('#txtVehicleCategory').val(vehicle_category);
+        $('#txtRemarks').val(remarks);
+        $('#txtStatus').val(status);
+    });
 
     $('#btnSearchEmploys').on('click', function() {
         const searchQuery = $('#txtSearchEmploys').val();
@@ -48,11 +125,11 @@ $(document).ready(function(){
         var status = $('#txtStatus').val();
 
         var isAvailable = status === "Available";
-        var role = isAvailable ? $('#txtVehicleRole').val() : "Not Available";
-        var first_name = isAvailable ? $('#txtVehicleFirstName').val() : "Not Available";
-        var phone_no = isAvailable ? $('#txtVehiclePhoneNumber').val() : "Not Available";
-        var email = isAvailable ? $('#txtVehicleEmail').val() : "Not Available";
         var staff_id = isAvailable ? $('#txtVehicleMemberID').val() : null;
+        var first_name = isAvailable ? $('#txtVehicleFirstName').val() : "*--*";
+        var role = isAvailable ? $('#txtVehicleRole').val() : "*--*";
+        var phone_no = isAvailable ? $('#txtVehiclePhoneNumber').val() : "*--*";
+        var email = isAvailable ? $('#txtVehicleEmail').val() : "*--*";
 
         const vehicleData = {
             vehicle_code:vehicle_code,

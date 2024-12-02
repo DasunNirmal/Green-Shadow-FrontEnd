83b84b097cfd5add6d65bb10a294c8f2d5540673
DasunNirmal <dasun200301@gmail.com>
2024-12-02 19:05:23 +0530

    save done

--- controller/VehicleController.js
+++ controller/VehicleController.js
@@ -38,4 +38,52 @@ $(document).ready(function(){
             }
         });
     }
+
+    $('#save-vehicles').on('click', () => {
+        var vehicle_code = $('#txtVehicleCode').val();
+        var license_plate = $('#txtLicensePlate').val();
+        var fuel_type = $('#txtFuelType').val();
+        var vehicle_category = $('#txtVehicleCategory').val();
+        var remarks = $('#txtRemarks').val();
+        var status = $('#txtStatus').val();
+
+        var isAvailable = status === "Available";
+        var role = isAvailable ? $('#txtVehicleRole').val() : "Not Available";
+        var first_name = isAvailable ? $('#txtVehicleFirstName').val() : "Not Available";
+        var phone_no = isAvailable ? $('#txtVehiclePhoneNumber').val() : "Not Available";
+        var email = isAvailable ? $('#txtVehicleEmail').val() : "Not Available";
+        var staff_id = isAvailable ? $('#txtVehicleMemberID').val() : null;
+
+        const vehicleData = {
+            vehicle_code:vehicle_code,
+            license_plate:license_plate,
+            fuel_type:fuel_type,
+            role:role,
+            remarks:remarks,
+            vehicle_category:vehicle_category,
+            status:status,
+            first_name:first_name,
+            phone_no:phone_no,
+            email:email,
+            staff_id:staff_id
+        };
+
+        const vehicleJSON = JSON.stringify(vehicleData);
+        console.log(vehicleJSON);
+
+        $.ajax({
+            url: 'http://localhost:8081/greenShadow/api/v1/vehicle',
+            type: 'POST',
+            data: vehicleJSON,
+            contentType: 'application/json',
+            success: (res) => {
+                console.log(JSON.stringify(res));
+                console.log("Vehicle saved successfully.");
+            },
+            error: (err) => {
+                console.error("Error saving vehicle:", err);
+            }
+        });
+    });
+
 });

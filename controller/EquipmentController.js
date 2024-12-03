b269423561ea51254260a2e357c36e50d00bdd30
DasunNirmal <dasun200301@gmail.com>
2024-12-03 21:18:46 +0530

    search staff done

---
+++ controller/EquipmentController.js
@@ -0,0 +1,41 @@
+$(document).ready(function(){
+
+
+    $('#btnSearchEmployees').on('click', function() {
+        const searchQuery = $('#txtSearchEmployees').val();
+        searchStaffByID(searchQuery);
+    });
+
+    function searchStaffByID(query) {
+        const staff_id = query.toLowerCase();
+
+        $.ajax({
+            url: 'http://localhost:8081/greenShadow/api/v1/staff?staff_id=' + staff_id,
+            type: 'GET',
+            dataType: 'json',
+            success: (staffResponse) => {
+                console.log('staff data:', staffResponse);
+                for (let i = 0; i < staffResponse.length; i++) {
+                    if (staff_id === staffResponse[i].staff_id) {
+                        var staff = staffResponse[i];
+                        break;
+                    }
+                }
+
+                if (staff) {
+                    console.log('Staff retrieved successfully:', staff);
+                    $('#txtMemberID-equipment').val(staff.staff_id);
+                    $('#txtFirstName-equipment').val(staff.first_name);
+                    $('#txtRole-equipment').val(staff.role);
+                    $('#txtPhoneNumber-equipment').val(staff.phone_no);
+                    $('#txtSearchEmployees').val("");
+                } else {
+                    console.error('Staff not found');
+                }
+            },
+            error: function(error) {
+                console.error('Error searching field:', error);
+            }
+        });
+    }
+});

d3cee1f0b9c1bcb9af78cee0c9ac0a00a05b56d6
DasunNirmal <dasun200301@gmail.com>
2024-12-05 01:12:09 +0530

    field log save done

--- controller/FieldLogController.js
+++ controller/FieldLogController.js
@@ -37,4 +37,57 @@ $(document).ready(function () {
             }
         });
     }
+
+    $('#save-field-logs').on('click', () => {
+        var log_code = $('#txtLogCode').val();
+        var img = $('#txtLogImage').prop('files')[0];
+        var details = $('#txtFieldDetails').val();
+        var log_date = $('#txtLogDate').val();
+
+        var field_code = $('#txtFieldCodeLogs').val();
+        var field_name = $('#txtFieldNameLogs').val();
+        var field_location = $('#txtFieldLocationLogs').val();
+
+        var logData = new FormData();
+        logData.append('log_code', log_code);
+        logData.append('img', img);
+        logData.append('details', details);
+        logData.append('log_date', log_date);
+        logData.append('field_code', field_code);
+        logData.append('field_name', field_name);
+        logData.append('field_location', field_location);
+
+        $.ajax({
+            url: 'http://localhost:8081/greenShadow/api/v1/logs',
+            type: 'POST',
+            data: logData,
+            mimeType: 'multipart/form-data',
+            contentType: false,
+            processData: false,
+            success: (response) => {
+                console.log('Log saved successfully:', response);
+                saveDetails(logData);
+            },
+            error: function(error) {
+                console.error('Error saving log:', error);
+            }
+        });
+    });
+
+    function saveDetails(logData) {
+        $.ajax({
+            url: 'http://localhost:8081/greenShadow/api/v1/fieldLogs',
+            type: 'POST',
+            data: logData,
+            mimeType: 'multipart/form-data',
+            contentType: false,
+            processData: false,
+            success: (response) => {
+                console.log('Log saved successfully:', response);
+            },
+            error: function(error) {
+                console.error('Error saving log:', error);
+            }
+        });
+    }
 });

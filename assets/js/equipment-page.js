027d3f0faf650bee93b0b3d66269efc1c50fcbf2
DasunNirmal <dasun200301@gmail.com>
2024-12-04 22:45:21 +0530

    small changes

--- assets/js/equipment-page.js
+++ assets/js/equipment-page.js
@@ -1,22 +1,30 @@
 $(document).ready(function () {
-    $('#txtStatus').on('click',  () => {
-        var status = $('#txtStatus').val();
+    $('#txtEquipmentStatus').on('click',  () => {
+        var status = $('#txtEquipmentStatus').val();
         var isAvailable = status === "Available";
 
-        var role = $('#txtVehicleRole');
-        var first_name = $('#txtVehicleFirstName');
-        var phone_no = $('#txtVehiclePhoneNumber');
-        var email = $('#txtVehicleEmail');
-        var staff_id = $('#txtVehicleMemberID');
-        var search = $('#txtSearchEmploys');
-        var btn = $('#btnSearchEmploys');
+        var staff_id = $('#txtMemberID-equipment');
+        var first_name = $('#txtFirstName-equipment');
+        var role = $('#txtRole-equipment');
+        var phone_no = $('#txtPhoneNumber-equipment');
+        var field_code = $('#txtFieldCode');
+        var field_name = $('#txtFieldName-equipment');
+        var field_location = $('#txtFieldLocation-equipment');
+        var search_employees = $('#txtSearchEmployees');
+        var search_fields = $('#txtSearchFields-equipment');
+        var emp_btn = $('#btnSearchEmployees');
+        var field_btn = $('#btnSearchFields-equipment');
 
-        role.prop('disabled', !isAvailable);
-        first_name.prop('disabled', !isAvailable);
-        phone_no.prop('disabled', !isAvailable);
-        email.prop('disabled', !isAvailable);
         staff_id.prop('disabled', !isAvailable);
-        search.prop('disabled', !isAvailable);
-        btn.prop('disabled', !isAvailable);
+        first_name.prop('disabled', !isAvailable);
+        role.prop('disabled', !isAvailable);
+        phone_no.prop('disabled', !isAvailable);
+        field_code.prop('disabled', !isAvailable);
+        field_name.prop('disabled', !isAvailable);
+        field_location.prop('disabled', !isAvailable);
+        search_employees.prop('disabled', !isAvailable);
+        search_fields.prop('disabled', !isAvailable);
+        emp_btn.prop('disabled', !isAvailable);
+        field_btn.prop('disabled', !isAvailable);
     });
 });

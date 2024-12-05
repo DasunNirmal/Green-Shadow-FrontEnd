149e8b47733cfede8c9550d93a9b1607fed41b31
DasunNirmal <dasun200301@gmail.com>
2024-12-05 16:19:42 +0530

    save done

--- controller/CropLogController.js
+++ controller/CropLogController.js
@@ -37,4 +37,56 @@ $(document).ready(function () {
             }
         });
     }
+
+    $('#save-crop-logs').on('click', () => {
+        var log_code = $('#txtLogCodeCrop').val();
+        var img = $('#txtLogImageCrop').prop('files')[0];
+        var details = $('#txtCropDetails').val();
+        var log_date = $('#txtLogDateCrop').val();
+
+        var crop_code = $('#txtCropCodeLogs').val();
+        var crop_name = $('#txtCropNameLogs').val();
+
+        var logData = new FormData();
+        logData.append('log_code', log_code);
+        logData.append('img', img);
+        logData.append('details', details);
+        logData.append('log_date', log_date);
+        logData.append('code', crop_code);
+        logData.append('name', crop_name);
+        logData.append('additional', crop_name);
+
+        $.ajax({
+            url: 'http://localhost:8081/greenShadow/api/v1/logs',
+            type: 'POST',
+            data: logData,
+            mimeType: 'multipart/form-data',
+            contentType: false,
+            processData: false,
+            success: (response) => {
+                console.log('Log saved successfully:', response);
+                saveDetails(logData);
+            },
+            error: function(error) {
+                console.error('Error saving log:', error);
+            }
+        });
+    });
+
+    function saveDetails(logData) {
+        $.ajax({
+            url: 'http://localhost:8081/greenShadow/api/v1/cropLogs',
+            type: 'POST',
+            data: logData,
+            mimeType: 'multipart/form-data',
+            contentType: false,
+            processData: false,
+            success: (response) => {
+                console.log('Log saved successfully:', response);
+            },
+            error: function(error) {
+                console.error('Error saving crop log:', error);
+            }
+        });
+    }
 });

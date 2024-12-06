d5d48caff8a7b1c4910319552b3a1eff20a33e68
DasunNirmal <dasun200301@gmail.com>
2024-12-06 10:43:24 +0530

    updated

--- controller/CropController.js
+++ controller/CropController.js
@@ -1,56 +1,54 @@
-$(document).ready(function () {
-    var recordIndexCrops;
-    loadCropTable();
+ var recordIndexCrops;
 
-    function clearFields() {
-        $('#txtCropCode').val("");
-        $('#txtCommonName').val("");
-        $('#txtScientificName').val("");
-        $('#txtCategory').val("");
-        $('#txtCropImage').val("");
-        $('#txtSeason').val("");
-        $('#txtSearchField').val("");
-        $('#txtCropFieldName').val("");
-    }
+function clearFields() {
+    $('#txtCropCode').val("");
+    $('#txtCommonName').val("");
+    $('#txtScientificName').val("");
+    $('#txtCategory').val("");
+    $('#txtCropImage').val("");
+    $('#txtSeason').val("");
+    $('#txtSearchField').val("");
+    $('#txtCropFieldName').val("");
+}
 
-    function loadCropTable() {
-        $("#crops-table-tb").empty();
+export function loadCropTable() {
+    $("#crops-table-tb").empty();
 
-        $.ajax({
-            url: 'http://localhost:8081/greenShadow/api/v1/crop',
-            type: 'GET',
-            dataType: 'json',
-            success: function(res) {
-                console.log(res);
-                if (Array.isArray(res)) {
-                    res.forEach(function(crop) {
-                        var cropRecord = `
-                        <tr>
-                            <td class="c-crop_code">${crop.crop_code}</td>
-                            <td class="c-common_name">${crop.common_name}</td>
-                            <td class="c-scientific_name">${crop.scientific_name}</td>
-                            <td class="c-category">${crop.category}</td>
-                            <td class="c-season">${crop.season}</td>
-                            <td class="c-field_code">${crop.field_code}</td>
-                            <td class="c-img"><img src="data:image/png;base64,${crop.img}" width="150px"></td>
+    $.ajax({
+        url: 'http://localhost:8081/greenShadow/api/v1/crop',
+        type: 'GET',
+        dataType: 'json',
+        success: function(res) {
+            console.log(res);
+            if (Array.isArray(res)) {
+                res.forEach(function(crop) {
+                    var cropRecord = `
+                       <tr>
+                           <td class="c-crop_code">${crop.crop_code}</td>
+                           <td class="c-common_name">${crop.common_name}</td>
+                           <td class="c-scientific_name">${crop.scientific_name}</td>
+                           <td class="c-category">${crop.category}</td>
+                           <td class="c-season">${crop.season}</td>
+                           <td class="c-field_code">${crop.field_code}</td>
+                           <td class="c-img"><img src="data:image/png;base64,${crop.img}" width="150px"></td>
                         </tr>`;
-                        $('#crops-table-tb').append(cropRecord);
-                    });
-                    let count = 0;
-                    for (let i = 0; i < res.length; i++) {
-                        if (res[i] != null) {
-                            count++;
-                        }
+                    $('#crops-table-tb').append(cropRecord);
+                });
+                let count = 0;
+                for (let i = 0; i < res.length; i++) {
+                    if (res[i] != null) {
+                        count++;
                     }
-                } else {
-                    console.log('No crop data found or incorrect response format.');
                 }
+            } else {
+                console.log('No crop data found or incorrect response format.');
+            }
             },
             error: function(res) {
                 console.error('Error loading crop data:', res);
-            }
-        });
-    }
+        }
+    });
+}
 
     $('#btnSearchField').on('click', function() {
         const searchQuery = $('#txtSearchField').val();
@@ -250,4 +248,3 @@ $(document).ready(function () {
     $('#clear-crops').on('click', () => {
         clearFields();
     });
-});
